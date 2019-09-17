dbf2be4981eca3081ad80b082c9f0e1f1f9f26bd
Ali Shakoori <alishakoori89@gmail.com>
2019-09-17 14:40:13 +0430

    edit code

--- GUI.py
+++ GUI.py
@@ -12,6 +12,7 @@ class DepositForm(tk.Frame):
         self.master = master
         tk.Frame.__init__(self, self.master, **kwargs)
         deposit_window = tk.Toplevel(self.master)
+        self.sql_obj=SQLHandler()
         deposit_window.title("deposit box")
         deposit_window.geometry("400x150")
         Label(deposit_window, text="ID number for deposit: ").grid(row=0, column=0)
@@ -27,9 +28,9 @@ class DepositForm(tk.Frame):
         sheet=sheet_field.get()
         sheet=int(sheet)
         id_search=id_field.get()
-        if SQLHandler.open_search(self, id_search):
-            intended_suply=SQLHandler.open_search(self, id_search)
-            SQLHandler.add_sheets(self, intended_suply,id_search,sheet)
+        if self.sql_obj.open_search(id_search):
+            intended_suply=self.sql_obj.open_search(id_search)
+            self.sql_obj.add_sheets(intended_suply,id_search,sheet)
             showinfo("notification box", "successfully operation")
         else:
             showinfo("notification box", "failed operation because customer not found ")
@@ -39,6 +40,7 @@ class DumpForm(tk.Frame):
         self.master=master
         tk.Frame.__init__(self,self.master,**kwargs)
         dump_window = tk.Toplevel(self.master)
+        self.sql_obj=SQLHandler()
         dump_window.title("dump box")
         dump_window.geometry("400x150")
         Label(dump_window,text="ID number for dump: ").grid(row=0, column=0)
@@ -54,9 +56,9 @@ class DumpForm(tk.Frame):
         sheet=sheet_field.get()
         sheet=int(sheet)
         id_search=id_field.get()
-        if SQLHandler.open_search(self, id_search):
-            intended_suply=SQLHandler.open_search(self, id_search)
-            if SQLHandler.sub_sheets(self,intended_suply,id_search,sheet):
+        if self.sql_obj.open_search(self, id_search):
+            intended_suply=self.sql_obj.open_search(self, id_search)
+            if self.sql_obj.sub_sheets(self,intended_suply,id_search,sheet):
                 showinfo("notification box", "successfully operation")
             else:
                 showinfo("notification box", "failed operation because not enough money ")
@@ -68,6 +70,7 @@ class WithdrawForm(tk.Frame):
         self.master=master
         tk.Frame.__init__(self,self.master,**kwargs)
         withdraw_window = tk.Toplevel(self.master)
+        self.sql_obj=SQLHandler()
         withdraw_window.title("dump box")
         withdraw_window.geometry("400x150")
         Label(withdraw_window,text="ID number for withdraw: ").grid(row=0, column=0)
@@ -78,7 +81,7 @@ class WithdrawForm(tk.Frame):
 
     def withdraw(self,id_field):
         id_search=id_field.get()
-        if SQLHandler.withdraw(self, id_search):
+        if self.sql_obj.withdraw(self, id_search):
             showinfo("notification box", "successfully operation")
         else:
             showinfo("notification box", "failed operation because customer not found ")
@@ -88,8 +91,10 @@ class ShowForm(tk.Frame):
         self.master=master
         tk.Frame.__init__(self,self.master,**kwargs)
         show_window = tk.Toplevel(self.master)
+        self.sql_obj=SQLHandler()
         show_window.title("show box")
-        list_name=SQLHandler.show(self)
+    # def show(self):
+        list_name=self.sql_obj.show()
         n=len(list_name)
         for i in range(0,n):
             Label(show_window,text=list_name[i]).grid()
@@ -107,85 +112,90 @@ def create_show_list(args,master=None):
     main_gui = ShowForm(master)
 
 class MainGUI:
-    master = tk.Tk()
-    master.geometry("400x200")
-    master.title("Operation Box")
-    name_field = Entry(master, bg='light gray', width=30, bd=2, selectborderwidth=5)
-    name_field.grid(row=0, column=3)
+    def __init__(self):
+        self.master = tk.Tk()
+        self.master.geometry("400x200")
+        self.master.title("Operation Box")
+        self.sql_obj=SQLHandler()
+        
+        name_field = Entry(self.master, bg='light gray', width=30, bd=2, selectborderwidth=5)
+        name_field.grid(row=0, column=3)
 
-    last_name_field = Entry(master, bg='light gray', width=30, bd=2, selectborderwidth=5)
-    last_name_field.grid(row=1, column=3)
+        last_name_field = Entry(self.master, bg='light gray', width=30, bd=2, selectborderwidth=5)
+        last_name_field.grid(row=1, column=3)
 
-    id_field = Entry(master, bg='light gray', width=30, bd=2, selectborderwidth=5)
-    id_field.grid(row=2, column=3)
+        id_field = Entry(self.master, bg='light gray', width=30, bd=2, selectborderwidth=5)
+        id_field.grid(row=2, column=3)
 
-    account_number_field = Entry(master, bg='light gray', width=30, bd=2, selectborderwidth=5)
-    account_number_field.grid(row=3, column=3)
+        account_number_field = Entry(self.master, bg='light gray', width=30, bd=2, selectborderwidth=5)
+        account_number_field.grid(row=3, column=3)
 
-    supply_field = Entry(master, bg='light gray', width=30, bd=2, selectborderwidth=5)
-    supply_field.grid(row=4, column=3)
+        supply_field = Entry(self.master, bg='light gray', width=30, bd=2, selectborderwidth=5)
+        supply_field.grid(row=4, column=3)
 
-    name_label = Label(master, text="Name: ")
-    name_label.grid(row=0, column=0, sticky=W)
+        name_label = Label(self.master, text="Name: ")
+        name_label.grid(row=0, column=0, sticky=W)
 
-    last_name_label = Label(master, text="LastName: ")
-    last_name_label.grid(row=1, column=0, sticky=W)
+        last_name_label = Label(self.master, text="LastName: ")
+        last_name_label.grid(row=1, column=0, sticky=W)
 
-    id_label = Label(master, text="ID Num: ")
-    id_label.grid(row=2, column=0, sticky=W)
+        id_label = Label(self.master, text="ID Num: ")
+        id_label.grid(row=2, column=0, sticky=W)
 
-    account_number_label = Label(master, text="Bank Account Number: ")
-    account_number_label.grid(row=3, column=0, sticky=W)
+        account_number_label = Label(self.master, text="Bank Account Number: ")
+        account_number_label.grid(row=3, column=0, sticky=W)
 
-    supply_label = Label(master, text="supply: ")
-    supply_label.grid(row=4, column=0, sticky=W)
+        supply_label = Label(self.master, text="supply: ")
+        supply_label.grid(row=4, column=0, sticky=W)
 
-    def add_button(self, name_field, last_name_field, id_field, bank_account_field, supply_field):
- 
-        try:
-            id_field=int(id_field.get())
-        except ValueError:
-            showinfo("notification box", "please input true data type and enter ID number again")
-        try:
-            name_field=str(name_field.get())
-        except ValueError:
-            showinfo("notification box", "please input true data type and enter name again")
-        try:
-            last_name_field=str(last_name_field.get())
-        except ValueError:
-            showinfo("notification box", "please input true data type and enter lastname again")
-        try:
-            bank_account_field=int(bank_account_field.get())
-        except ValueError:
-            showinfo("notification box", "please input true data type and enter bank account number again ")
-        try:
-            supply_field=int(supply_field.get())
-        except ValueError:
-            showinfo("notification box", "please input true data type and enter supply again")
-
-        data_list=[id_field,name_field,last_name_field,bank_account_field,supply_field]
-
-        SQLHandler.insert_table(self,data_list)
-
-
-    create_add_window_action_with_arg = partial(add_button, master, name_field, last_name_field, id_field,
-                                                account_number_field,
-                                                supply_field)
-    Button(master, text='Add', command=create_add_window_action_with_arg).place(x=10, y=145)
-
-
-    create_deposit_window_action_with_arg = partial(create_deposit_window, master)
-    Button(master, text='Deposit', command=create_deposit_window_action_with_arg).place(x=45, y=145)
+        def add_button(self, name_field, last_name_field, id_field, bank_account_field, supply_field):
     
-    create_dump_window_action_with_arg = partial(create_dump_window,master)
-    Button(master, text='Dump', command=create_dump_window_action_with_arg).place(x=98, y=145)
+            try:
+                id_field=int(id_field.get())
+            except ValueError:
+                showinfo("notification box", "please input true data type and enter ID number again")
+            try:
+                name_field=str(name_field.get())
+            except ValueError:
+                showinfo("notification box", "please input true data type and enter name again")
+            try:
+                last_name_field=str(last_name_field.get())
+            except ValueError:
+                showinfo("notification box", "please input true data type and enter lastname again")
+            try:
+                bank_account_field=int(bank_account_field.get())
+            except ValueError:
+                showinfo("notification box", "please input true data type and enter bank account number again ")
+            try:
+                supply_field=int(supply_field.get())
+            except ValueError:
+                showinfo("notification box", "please input true data type and enter supply again")
 
-    create_withdraw_window_action_with_arg = partial(create_withdraw_window,master)
-    Button(master, text="Withdraw", command=create_withdraw_window_action_with_arg).place(x=143,y=145)
+            data_list=[id_field,name_field,last_name_field,bank_account_field,supply_field]
 
-    create_show_window_action_with_arg = partial(create_show_list,master)
-    Button(master,text="Show",command=create_show_window_action_with_arg).place(x=207,y=145)
-    
-    master.mainloop()
+            SQLHandler.insert_table(self,data_list)
+            showinfo("notification box", "customer adding successfully")
 
-main = MainGUI
+
+        create_add_window_action_with_arg = partial(add_button, self.master, name_field, last_name_field, id_field,
+                                                    account_number_field,
+                                                    supply_field)
+        Button(self.master, text='Add', command=create_add_window_action_with_arg).place(x=10, y=145)
+
+
+        create_deposit_window_action_with_arg = partial(create_deposit_window, self.master)
+        Button(self.master, text='Deposit', command=create_deposit_window_action_with_arg).place(x=45, y=145)
+        
+        create_dump_window_action_with_arg = partial(create_dump_window,self.master)
+        Button(self.master, text='Dump', command=create_dump_window_action_with_arg).place(x=98, y=145)
+
+        create_withdraw_window_action_with_arg = partial(create_withdraw_window,self.master)
+        Button(self.master, text="Withdraw", command=create_withdraw_window_action_with_arg).place(x=143,y=145)
+
+        create_show_window_action_with_arg = partial(create_show_list,self.master)
+        Button(self.master,text="Show",command=create_show_window_action_with_arg).place(x=207,y=145)
+        
+        # self.master.mainloop()
+
+main=MainGUI()
+main.master.mainloop()
--- bank_db.py
+++ bank_db.py
@@ -1,51 +1,61 @@
 import sqlite3
 
-conn=sqlite3.connect('BankDB.db')
-c=conn.cursor()
+
 global i
 i=0
 
 class SQLHandler:      
-        
+    def __init__(self,database='BankDB.db'):
+        self.database = database
+        self.conn=sqlite3.connect(self.database)
+        self.c=self.conn.cursor()
+
     def insert_table(self,data_list):
         global i
-        print(data_list)
-        print(data_list[0])
-        c.execute('CREATE TABLE IF NOT EXISTS DataTable(ID INTEGER PRIMARY KEY,FName TEXT(20),LName TEXT(30),Account_Number INTEGER,Suply INTEGER)')
-        c.execute("INSERT INTO DataTable (ID,FName,LName,Account_Number,Suply) VALUES (?,?,?,?,?)",(data_list[i],data_list[i+1],data_list[i+2],data_list[i+3],data_list[i+4]))
-        conn.commit()
+        self.c.execute('SELECT ID FROM DataTable')
+        x=self.c.fetchall()
+        # print(x)
+        # b=str(data_list[i])
+        # t=tuple(str(data_list[i]))
+        print(t)
+        if t in x:
+            return False
+        else:
+            self.c.execute('CREATE TABLE IF NOT EXISTS DataTable(ID INTEGER PRIMARY KEY,FName TEXT(20),LName TEXT(30),Account_Number INTEGER,Suply INTEGER)')
+            self.c.execute("INSERT INTO DataTable (ID,FName,LName,Account_Number,Suply) VALUES (?,?,?,?,?)",(data_list[i],data_list[i+1],data_list[i+2],data_list[i+3],data_list[i+4]))
+            self.conn.commit()
 
     def open_search(self,id_search):
         query_string ="SELECT Suply FROM DataTable WHERE ID = (?)"
-        intended_suply=c.execute(query_string, (id_search,))
-        intended_suply=c.fetchone()
+        intended_suply=self.c.execute(query_string, (id_search,))
+        intended_suply=self.c.fetchone()
         return intended_suply
 
 
     def add_sheets(self,intended_suply,id_search,sheet):
         l=list(intended_suply)
         new_suply=l[0]+sheet
-        c.execute("UPDATE DataTable SET Suply = (?) WHERE ID = (?)" ,(new_suply,id_search,))
-        conn.commit()
+        self.c.execute("UPDATE DataTable SET Suply = (?) WHERE ID = (?)" ,(new_suply,id_search,))
+        self.conn.commit()
 
     def sub_sheets(self,intended_suply,id_search,sheet):
         l=list(intended_suply)
         new_suply=l[0]-sheet
         if new_suply>0 :
-            c.execute("UPDATE DataTable SET Suply = (?) WHERE ID = (?)" ,(new_suply,id_search,))
-            conn.commit()
+            self.c.execute("UPDATE DataTable SET Suply = (?) WHERE ID = (?)" ,(new_suply,id_search,))
+            self.conn.commit()
             return True
         else:
             return False
 
     def withdraw(self,id_search):
-        if c.execute("DELETE FROM DataTable WHERE ID=(?)",(id_search,)):
-            conn.commit()
+        if self.c.execute("DELETE FROM DataTable WHERE ID=(?)",(id_search,)):
+            self.conn.commit()
             return True
         else:
             return False
 
     def show(self):
-        c.execute("select * from DataTable")
-        data = c.fetchall() #data from database
+        self.c.execute("select * from DataTable")
+        data = self.c.fetchall() #data from database
         return data
